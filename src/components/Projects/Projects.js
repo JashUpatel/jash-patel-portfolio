c06bbd8365c70216ab7e8400c6583efc4bddd5c2
Jash Patel <41770078+JashUpatel@users.noreply.github.com>
2023-05-25 22:42:45 +0530

    Update Projects.js
    updating project.js to align source and code button links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,38 +1,67 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map((p, i) => {
-        return (
-          <BlogCard key={i}>
-          <Img src={p.image} />
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
             <TitleContent>
-              <HeaderThree title>{p.title}</HeaderThree>
+              <HeaderThree title>{title}</HeaderThree>
               <Hr />
             </TitleContent>
-            <CardInfo className="card-info">{p.description}</CardInfo>
+            <CardInfo>{description}</CardInfo>
             <div>
               <TitleContent>Stack</TitleContent>
               <TagList>
-                {p.tags.map((t, i) => {
-                  return <Tag key={i}>{t}</Tag>;
-                })}
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
               </TagList>
             </div>
             <UtilityList>
-              <ExternalLinks href={p.visit}>Code</ExternalLinks>
-              <ExternalLinks href={p.source}>Source</ExternalLinks>
+              {visit.trim().length > 0 ? (
+                <ExternalLinks href={visit} target="_blank">
+                  Source
+                </ExternalLinks>
+              ) : (
+                ""
+              )}
+
+              {source.trim().length > 0 ? (
+                <ExternalLinks href={source} target="_blank">
+                  Code
+                </ExternalLinks>
+              ) : (
+                ""
+              )}
             </UtilityList>
           </BlogCard>
-        );
-      })}
+        )
+      )}
     </GridContainer>
   </Section>
 );
